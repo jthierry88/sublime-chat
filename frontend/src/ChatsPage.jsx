32cafa64ba25dcd762a8e872ebf20b5df8df9a8a
James Thierry <jamesthierry07@gmail.com>
2023-05-28 15:41:29 -0500

    Install PrettyChat

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,15 +1,14 @@
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
 
 const ChatsPage = (props) => {
-    const chatProps = useMultiChatLogic(
-        'ec66ef14-94a7-4e45-b4a0-4df508262f29', 
-        props.user.username, 
-        props.user.secret
-    );
     return (
     <div style={{ height: '100vh' }}>
-        <MultiChatSocket {...chatProps} />
-        <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+        <PrettyChatWindow
+            projectId='ec66ef14-94a7-4e45-b4a0-4df508262f29'
+            username={props.user.username}
+            secret={props.user.secret}
+            style={{ height: '100%' }}
+        />
     </div>
     )
 }
